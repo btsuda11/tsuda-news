299370cefecf69e6fd791d906c9b800d477c11b8
Brendan <brendantsuda@gmail.com>
2023-01-24 13:43:18 -0800

    Complete search box

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Bars3Icon } from '@heroicons/react/24/solid';
 import Link from 'next/link';
 import NavLinks from './NavLinks';
+import SearchBox from './SearchBox';
 
 function Header() {
   return (
@@ -15,10 +16,11 @@ function Header() {
                 </h1>
             </Link>
             <div className='flex items-center justify-end space-x-2'>
-                <button className='hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800'>Subscribe Now</button>
+                <button className='hidden md:inline bg-slate-900 text-white px-4 lg:px-6 py-2 lg:py-3 rounded-full dark:bg-slate-800'>subscribe now</button>
             </div>
         </div>
         <NavLinks />
+        <SearchBox />
     </header>
   )
 }
